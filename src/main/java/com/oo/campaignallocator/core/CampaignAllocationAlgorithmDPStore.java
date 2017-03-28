e2140127dbbaae9902c06364264b8da51cff51e4
Akiz <shawpan.du@gmail.com>
2017-03-29 00:49:45 +0600

    doc: Add documentation of CampaignAllocationAlgorithmDPStore class

--- src/main/java/com/oo/campaignallocator/core/CampaignAllocationAlgorithmDPStore.java
+++ src/main/java/com/oo/campaignallocator/core/CampaignAllocationAlgorithmDPStore.java
@@ -5,10 +5,26 @@ import com.oo.campaignallocator.api.CampaignAllocationResponse;
 import java.lang.Math;
 import java.util.ArrayList;
 
+/**
+ * CampaignAllocationAlgorithmDPStore stores the vectors to compute optimal
+ * solution using 0-1 Knapsack DP algorithm
+ * @author Akiz
+ */
 public class CampaignAllocationAlgorithmDPStore {
+    /**
+     * Holds the revenue calculation data required for the DP algorithm
+     */
     public int dpVector[];
+
+    /**
+     * Holds index of picked campaign as value for the inventory as key
+     */
     public int pickedElementIndex[];
 
+    /**
+     * Creates the memory/store object for the DP algorithm
+     * @param size is the length of vectors 
+     */
     public CampaignAllocationAlgorithmDPStore(int size) {
         dpVector = new int[size];
         pickedElementIndex = new int[size];
