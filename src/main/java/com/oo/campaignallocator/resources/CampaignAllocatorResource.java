0a6630b274a99c309d595257e65175bf8c9ec236
Akiz <shawpan.du@gmail.com>
2017-03-29 00:54:18 +0600

    doc: Add documentation of CampaignAllocatorResource class

--- src/main/java/com/oo/campaignallocator/resources/CampaignAllocatorResource.java
+++ src/main/java/com/oo/campaignallocator/resources/CampaignAllocatorResource.java
@@ -7,16 +7,33 @@ import com.oo.campaignallocator.api.CampaignAllocationRequest;
 import com.oo.campaignallocator.core.CampaignAllocationAlgorithmInterface;
 import com.oo.campaignallocator.core.CampaignAllocationAlgorithmDP;
 
+/**
+ * CampaignAllocatorResource represents the public api for campaign allocation
+ * it consumes and produces json output
+ * @author Akiz
+ */
+
 @Path("/campaign-allocation")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class CampaignAllocatorResource {
+    /**
+     * Holds the campaign allocation algorthim object
+     */
     private CampaignAllocationAlgorithmInterface campaignAllocationAlgorithm;
 
+    /**
+     * Creates a resource object for the http protocol
+     */
     public CampaignAllocatorResource() {
         this.campaignAllocationAlgorithm = new CampaignAllocationAlgorithmDP();
     }
 
+    /**
+     * This is the POST api method for "/campaign-allocation" uri
+     * @param campaignAllocationRequest the input json object
+     * @return CampaignAllocationResponse the output json object
+     */
     @POST
     public CampaignAllocationResponse allocateCampaigns(CampaignAllocationRequest campaignAllocationRequest) {
         campaignAllocationAlgorithm.run(campaignAllocationRequest);
