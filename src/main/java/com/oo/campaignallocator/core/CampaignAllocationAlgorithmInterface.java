3f1808c3f973957b6fba9c7fb96aa8227fcdde03
Akiz <shawpan.du@gmail.com>
2017-03-29 00:52:32 +0600

    doc: Add documentation of CampaignAllocationAlgorithmInterface interface

--- src/main/java/com/oo/campaignallocator/core/CampaignAllocationAlgorithmInterface.java
+++ src/main/java/com/oo/campaignallocator/core/CampaignAllocationAlgorithmInterface.java
@@ -3,9 +3,23 @@ package com.oo.campaignallocator.core;
 import com.oo.campaignallocator.api.CampaignAllocationRequest;
 import com.oo.campaignallocator.api.CampaignAllocationResponse;
 
+/**
+ * CampaignAllocationAlgorithmInterface is the contact of any algorithm that
+ * can be used for campaign allocation in this application
+ * @author Akiz
+ */
 public interface CampaignAllocationAlgorithmInterface {
-
+    /**
+     * This method provides the signature of the method to run the algorithm
+     * in any class that can be used as campaign allocation algorithm
+     * @param campaignAllocationRequest is the input object
+     */
     public void run(CampaignAllocationRequest campaignAllocationRequest);
 
+    /**
+     * This method provides the signature of the method to get the final output object
+     * in any class that can be used as campaign allocation algorithm
+     * @return CampaignAllocationResponse
+     */
     public CampaignAllocationResponse getCampaignAllocationResponse();
 }
